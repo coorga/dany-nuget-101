f8e533ec38b68a92073b57fac74cc35c9647b2a3
danylo.liubymyi <danylo.liubymyi@co-orga.de>
2025-10-30 09:51:52 +0100

    ..

--- NuGetSample/MyCalculator/Calculator.cs
+++ NuGetSample/MyCalculator/Calculator.cs
@@ -2,6 +2,17 @@
 
 public class Calculator     
 {
-    public double Add(double a, double b) => a + b;
+    /// <summary>
+    /// Adds two numbers
+    /// </summary>
+    /// <param name="a">number 1</param>
+    /// <param name="b">number 2</param>
+    /// <returns>result of adding number 1 and number 2</returns>
+    public double Add(double a, double b)
+    {
+        //Comment for testing
+        return a + b;
+    }
+
     public double Subtract(double a, double b) => a - b;
 }
